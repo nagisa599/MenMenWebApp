13a5eca70e26d8b250a69be79c8952f3444beabb
Kohei Komuro <82309237+k-sml@users.noreply.github.com>
2023-12-02 22:58:01 +0900

    homepage修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,14 @@ import Image from "next/image";
 import { ref, getStorage, getDownloadURL } from "firebase/storage";
 
 export default function Home() {
-  const [lunchTime, setLunchTime] = useState<string>('お休みです');
-  const [dinnerTime, setDinnerTime] = useState<string>('お休みです');
+  const [lunchTime, setLunchTime] = useState<string>('');
+  const [dinnerTime, setDinnerTime] = useState<string>('');
   const [imageUrls, setImageUrls] = useState<string[]>([]);
   const [ramenNames, setRamenNames] = useState<string[]>([]);
-  const weekdays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
   const today = new Date();
   const month = today.getMonth() + 1;
   const day = today.getDate();
   const year = today.getFullYear();
-  const dayName = weekdays[today.getDay()];
 
   useEffect(() => {
     const db = getFirestore();
@@ -30,11 +28,18 @@ export default function Home() {
 
         querySnapshot.forEach((doc) => {
           const data = doc.data();
-          if (data.schedule[dayName]?.lunch) {
-            setLunchTime(data.schedule[dayName].lunch);
-          }
-          if (data.schedule[dayName]?.dinner) {
-            setDinnerTime(data.schedule[dayName].dinner);
+          if (data.dayoff.includes(`${month}/${day}`)) {
+            setLunchTime('お休みです');
+            setDinnerTime('お休みです')
+          } else if (today.getDay() !== 0 && today.getDay() !== 6) {
+            setLunchTime(`${data.businessHours.weekday.lunch.start}~${data.businessHours.weekday.lunch.end}`)
+            setDinnerTime(`${data.businessHours.weekday.dinner.start}~${data.businessHours.weekday.dinner.end}`)
+          } else if (today.getDay() === 0) {
+            setLunchTime(`${data.businessHours.sunday.lunch.start}~${data.businessHours.sunday.lunch.end}`)
+            setDinnerTime(`${data.businessHours.sunday.dinner.start}~${data.businessHours.sunday.dinner.end}`)
+          } else {
+            setLunchTime(`${data.businessHours.saturday.lunch.start}~${data.businessHours.saturday.lunch.end}`)
+            setDinnerTime(`${data.businessHours.saturday.dinner.start}~${data.businessHours.saturday.dinner.end}`)
           }
         });
       } catch (error) {
@@ -43,17 +48,32 @@ export default function Home() {
     };
 
     const setTodayMenu = async () => {
+      today.setHours(0, 0, 0, 0); // 時間をリセットして日付のみに焦点を合わせる
+
       try {
-        const menuQuery = query(collection(db, 'ramens'), where('today', '==', true), orderBy('topping', 'desc'));
+        const storage = getStorage();
+        const menuQuery = query(collection(db, 'ramens'));
         const querySnapshot = await getDocs(menuQuery);
         const tempImageUrls = [];
         const tempRamenNames = [];
 
         for (const doc of querySnapshot.docs) {
           const data = doc.data();
-          const imageUrl = await getDownloadURL(ref(storage, data.imageURL));
-          tempImageUrls.push(imageUrl);
-          tempRamenNames.push(data.name);
+
+          if (data.today) {
+            const menuDate = new Date(data.today.toDate());
+            menuDate.setHours(0, 0, 0, 0); // Firestore のタイムスタンプから日付のみを取得
+
+            if (menuDate.getTime() === today.getTime()) {
+              const imageUrl = await getDownloadURL(ref(storage, data.imageURL));
+              tempImageUrls.push(imageUrl);
+              tempRamenNames.push(data.name);
+            }
+          } else {
+            const imageUrl = await getDownloadURL(ref(storage, data.imageURL));
+            tempImageUrls.push(imageUrl);
+            tempRamenNames.push(data.name);
+          }
         }
 
         setImageUrls(tempImageUrls);
@@ -63,6 +83,7 @@ export default function Home() {
       }
     };
 
+
     Promise.all([setHeaderTitle(), setTodayMenu()]);
   }, []);
 
@@ -70,7 +91,7 @@ export default function Home() {
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
-      <div className="mb-4">
+      <div className="divide-y divide-gray-200 mx-60 p-4">
         <div className="mb-8">
           <h2 className="text-lg font-semibold">本日({`${month}月${day}日`})の営業時間</h2>
           <div className="mt-2">
